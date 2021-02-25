576cadd98716dd80af21bef3e2e5ad28ad61b877
Coding-Ocean <eijo524@gmail.com>
2021-02-25 17:35:43 +0900

    save

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -2,13 +2,10 @@
 class VEHICLE {
 public:
     VEHICLE() {
-        Diameter = 50;
-        Pos.x = width / 2;
-        Pos.y = height / 2;
-        Vel.x = 0;
-        Vel.y = 0;
+        Pos.x = width / 2+10;
+        Pos.y = height / 2+10;
         MaxSpeed = 10;
-        MaxForce = 1;
+        MaxForce = 0.6f;
     }
     void chase(VECTOR2 targetPos) {
         //-----最終的に加速度を求める-----
@@ -16,28 +13,42 @@ public:
         //まず望まれるベクトルを求める
         VECTOR2 desiredVel = targetPos - Pos;
         float distance = length(desiredVel);
-        float speed = MaxSpeed;
-        if (distance < 100) {
-            //（100より近いなら）距離に応じてスピードを遅くする
-            speed = MaxSpeed * distance / 100;
+        if (distance >= 0.01) {
+            float speed = MaxSpeed;
+            if (distance < 100) {
+                //（100より近いなら）距離に応じてスピードを遅くする
+                speed = MaxSpeed * distance / 100;
+            }
+            //desiredVelを単位ベクトル化してspeedを掛ける
+            desiredVel *= speed / distance;
+
+            //加速度ベクトルを求める
+            Acc = desiredVel - Vel;
+            distance = length(Acc);
+            if (distance > MaxForce) {
+                Acc *= MaxForce / distance;
+            }
         }
-        fill(0);
-        text(speed, 0, 20);
-        //desiredVelを単位ベクトル化してspeedを掛ける
-        desiredVel *= speed / distance;
-      
-        //加速度ベクトルを求める
-        Acc = desiredVel - Vel;
-        distance = length(Acc);
-        text(distance, 0, 40);
-        if (distance > MaxForce) {
-            Acc *= MaxForce / distance;
+        else {
+            Acc *= 0;
+            Pos = targetPos;
         }
-        
     }
     void move() {
-        Pos += Vel;
         Vel += Acc;
+        Pos += Vel;
+        if (Acc.x == 0.0f && Acc.y == 0.0f) {
+            fill(255, 0, 0);
+        }
+        else {
+            fill(0);
+        }
+        text(Acc.x, 0, 20);
+        text(Acc.y, 0, 40);
+        text(Vel.x, 0, 80);
+        text(Vel.y, 0, 100);
+        text(Pos.x, 0, 140);
+        text(Pos.y, 0, 160);
     }
     void draw() {
         fill(255, 255, 60);
@@ -45,7 +56,7 @@ public:
         rectMode(CENTER);
         rect(Pos.x, Pos.y, 60,100,atan2(-Vel.x,Vel.y));
         strokeWeight(5);
-        line(Pos.x, Pos.y, Pos.x + Vel.x*2, Pos.y + Vel.y*2);
+        line(Pos.x, Pos.y, Pos.x + Vel.x*20, Pos.y + Vel.y*20);
     }
 private:
     float MaxSpeed;
@@ -53,24 +64,23 @@ private:
     VECTOR2 Pos;
     VECTOR2 Vel;
     VECTOR2 Acc;
-    float Diameter;
 };
+
 void gmain() {
     window(1000, 1000);
     VEHICLE vehicle;
-    float deg = 0;
+    //ターゲットポジションx,y
+    float x = width/2, y = height/2;
     angleMode(DEGREES);
     while (notQuit) {
         clear(90);
-        float x = cos(deg) * 400 + width / 2;
-        float y = -sin(deg * 2) * 400 + height / 2;
-        deg += 2;
+        if (isTrigger(MOUSE_LBUTTON)) {
+            x = mouseX;
+            y = mouseY;
+        }
         vehicle.chase(VECTOR2(x, y));
         vehicle.move();
-        line(0, height / 2, width, height / 2);
-        line(width / 2, 0, width / 2, height);
         vehicle.draw();
-        text(130.0 / 1700000, 0, 100);
     }
 }
 
